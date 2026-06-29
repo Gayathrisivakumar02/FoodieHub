351f86893267f37aefbe183f0525a2b3b30136b1
Gayathri <sivakumargayathri02@gmail.com>
2026-06-29 14:50:49 +0530

    FoodieHub

--- script.js
+++ script.js
@@ -19,3 +19,55 @@ function addToCart(foodName, price) {
         .innerHTML = total;
 
 }
+function placeOrder(){
+
+    let name =
+    document.getElementById("name").value;
+
+    let phone =
+    document.getElementById("phone").value;
+
+    let address =
+    document.getElementById("address").value;
+
+    let city =
+    document.getElementById("city").value;
+
+    let pincode =
+    document.getElementById("pincode").value;
+
+    let payment =
+    document.querySelector(
+        'input[name="payment"]:checked'
+    );
+
+    if(
+        name === "" ||
+        phone === "" ||
+        address === "" ||
+        city === "" ||
+        pincode === ""
+    ){
+
+        alert("Please fill all details!");
+        return;
+    }
+
+    if(payment === null){
+
+        alert("Please select a payment method!");
+        return;
+    }
+
+    alert(
+        "✅ Order Placed Successfully!\n\n" +
+        "Name: " + name +
+        "\nPhone: " + phone +
+        "\nAddress: " + address +
+        "\nCity: " + city +
+        "\nPincode: " + pincode +
+        "\nPayment: " + payment.value +
+        "\nTotal Amount: ₹" + total
+    );
+
+}
